1af301cff50520be9324636001f075f64c3a921a
fengxh <1010454733@qq.com>
2017-09-30 14:46:30 +0800

    files structure add some pictures

--- assets/js/jquery.slide.js
+++ assets/js/jquery.slide.js
@@ -4,14 +4,15 @@
 (function($){
     var Slide = function(ele, options){
         this._defaultOpt = {
-            imgNo: 5
+            percent: "%"
         };
         this.opts = $.extend({}, this._defaultOpt, options);
         this.$doc = $(document);
         this.$body = $("body");
         this.$ele = $(ele);
         this.$imgBox = this.$ele.children().eq(0);
-        this.$imgList = null;
+        this.$imgList = this.$imgBox.children();
+        this.imgNo = this.$imgList.length;
         this.$dotBox = this.$ele.children().eq(1);
         this.$dotList = null;
         this._init();
@@ -19,9 +20,13 @@
     Slide.prototype = {
         constructor: Slide,
         _init: function(){
-            
+            this.renderImgBox();
         },
-        
+        renderImgBox: function(){
+            console.log(1);
+            this.$imgBox.css({width: (this.imgNo * 100) + this.opts.percent});
+            this.$imgList.css({width: (100 / this.imgNo) + this.opts.percent});
+        }
     };
     $.fn.slide = function(options){
         return this.each(function(){
